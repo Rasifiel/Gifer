6a65981c14f5fbb0b3424256886f1f9636e74917
Rasifiel <Rasifiel@Laptop>
2019-10-06 05:35:24 +0200

    OSD support.

--- Gifer/Form1.cs
+++ Gifer/Form1.cs
@@ -1,7 +1,7 @@
-﻿using mrousavy;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Collections.Specialized;
+using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
@@ -20,6 +20,7 @@ namespace Gifer {
       RegisterHotKey(Handle, 1, System.Windows.Input.ModifierKeys.Control | System.Windows.Input.ModifierKeys.Alt | System.Windows.Input.ModifierKeys.Shift, KeyInterop.VirtualKeyFromKey(Key.A));
       RegisterHotKey(Handle, 2, System.Windows.Input.ModifierKeys.Control | System.Windows.Input.ModifierKeys.Alt | System.Windows.Input.ModifierKeys.Shift, KeyInterop.VirtualKeyFromKey(Key.S));
       RegisterHotKey(Handle, 3, System.Windows.Input.ModifierKeys.Control | System.Windows.Input.ModifierKeys.Alt | System.Windows.Input.ModifierKeys.Shift, KeyInterop.VirtualKeyFromKey(Key.Z));
+      osd.Show();
     }
 
     private void CutGif(int from, int to, String filePath) {
@@ -36,8 +37,10 @@ namespace Gifer {
   .SetOutput(resultPath).SetOverwriteOutput(true);
       var result = conv.Start().Result;
       if (!result.Success) {
-        MessageBox.Show("Failed!", "Conversion");
+        osd.ShowOSD("Building gif failed", 1000);
+        return;
       }
+      osd.ShowOSD("Finished building gif", 1000);
       StringCollection resultList = new StringCollection();
       resultList.Add(resultPath);
       Clipboard.SetFileDropList(resultList);
@@ -47,6 +50,7 @@ namespace Gifer {
     int end = -1;
     String fileName = "";
     VideoPlayerAPI api = new MPCAPI();
+    OSDWindow osd = new OSDWindow();
 
     protected override void WndProc(ref Message m) {
       if (m.Msg == 0x0312) {
@@ -58,6 +62,7 @@ namespace Gifer {
                 end = -1;
               }
               start = state.position;
+              osd.ShowOSD("Marked start position " + TimeSpan.FromMilliseconds(start).ToString(), 1000);
             }
             break;
           case 2: {
@@ -67,6 +72,7 @@ namespace Gifer {
                 start = -1;
               }
               end = state.position;
+              osd.ShowOSD("Marked end position " + TimeSpan.FromMilliseconds(end).ToString(), 1000);
             }
             break;
           case 3: {
@@ -78,6 +84,58 @@ namespace Gifer {
         }
       }
       base.WndProc(ref m);
+    }
+
+    private void button1_Click(object sender, EventArgs e) {
+      osd.ShowOSD("Test!"+new Random().Next(0,10000), 2000);
+    }
+  }
+
+  internal class OSDWindow : Form {
+    public OSDWindow() {
+      this.Bounds = Screen.PrimaryScreen.Bounds;
+      this.TopMost = true; // make the form always on top
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None; // hidden border
+      this.WindowState = FormWindowState.Maximized; // maximized
+      this.MinimizeBox = this.MaximizeBox = false; // not allowed to be minimized
+      this.MinimumSize = this.MaximumSize = this.Size; // not allowed to be resized
+      this.TransparencyKey = this.BackColor = Color.Black; // the color key to transparent, choose a color that you don't use
+      timer.Tick += delegate { if (needToClean && DateTime.Now >= timeToClean) { message = ""; needToClean = false; Refresh(); } };
+      timer.Interval = 10;
+      timer.Start();
+    }
+    protected override CreateParams CreateParams {
+      get {
+        CreateParams cp = base.CreateParams;
+        // Set the form click-through
+        cp.ExStyle |= 0x80000 /* WS_EX_LAYERED */ | 0x20 /* WS_EX_TRANSPARENT */;
+        return cp;
+      }
+    }
+
+    private String message = "";
+    private Font osdFont = new Font("Verdana", 26, FontStyle.Bold);
+    private Timer timer = new Timer();
+    private bool needToClean = false;
+    private DateTime timeToClean;
+
+    public void ShowOSD(String message, int time) {
+      timeToClean = DateTime.Now.AddMilliseconds(time);
+      needToClean = true;
+      this.message = message;
+      Refresh();
+    }
+
+    protected override void OnPaint(PaintEventArgs e) {
+      base.OnPaint(e);
+      Graphics g = e.Graphics;
+      g.TextRenderingHint =
+   System.Drawing.Text.TextRenderingHint.AntiAlias;
+      GraphicsPath gp = new GraphicsPath();
+      gp.AddString(message, osdFont.FontFamily, (int)osdFont.Style, g.DpiY * osdFont.SizeInPoints / 72, new Point(0, 0), StringFormat.GenericDefault);
+      g.SmoothingMode = SmoothingMode.HighQuality;
+      g.Clear(Color.Black);
+      g.FillPath(Brushes.Green, gp);
     }
   }
 }
