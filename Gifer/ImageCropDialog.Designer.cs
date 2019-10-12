11a41b6821fbcd67f4fd4904b8815fa96cd8c5b5
Rasifiel <Rasifiel@Laptop>
2019-10-12 08:26:07 +0200

    Crop mode.

---
+++ Gifer/ImageCropDialog.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace Gifer {
+  partial class ImageCropDialog {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing) {
+      if (disposing && (components != null)) {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent() {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ImageCropDialog));
+      this.imageCropBox = new Cyotek.Windows.Forms.ImageBox();
+      this.SuspendLayout();
+      // 
+      // imageCropBox
+      // 
+      this.imageCropBox.AllowZoom = false;
+      this.imageCropBox.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.imageCropBox.DropShadowSize = 0;
+      this.imageCropBox.Location = new System.Drawing.Point(0, 0);
+      this.imageCropBox.Margin = new System.Windows.Forms.Padding(0);
+      this.imageCropBox.Name = "imageCropBox";
+      this.imageCropBox.SelectionMode = Cyotek.Windows.Forms.ImageBoxSelectionMode.Rectangle;
+      this.imageCropBox.Size = new System.Drawing.Size(1262, 673);
+      this.imageCropBox.SizeMode = Cyotek.Windows.Forms.ImageBoxSizeMode.Fit;
+      this.imageCropBox.TabIndex = 0;
+      this.imageCropBox.KeyUp += new System.Windows.Forms.KeyEventHandler(this.ImageCropDialog_KeyUp);
+      // 
+      // ImageCropDialog
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(1262, 673);
+      this.ControlBox = false;
+      this.Controls.Add(this.imageCropBox);
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+      this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+      this.Name = "ImageCropDialog";
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+      this.Text = "ImageCropDialog";
+      this.TopMost = true;
+      this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.ImageCropDialog_KeyUp);
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    public Cyotek.Windows.Forms.ImageBox imageCropBox;
+  }
+}
--- Gifer/MainForm.cs
+++ Gifer/MainForm.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Specialized;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
@@ -18,20 +19,23 @@ namespace Gifer {
       RegisterHotKey(Handle, 1, System.Windows.Input.ModifierKeys.Control | System.Windows.Input.ModifierKeys.Alt | System.Windows.Input.ModifierKeys.Shift, KeyInterop.VirtualKeyFromKey(Key.A));
       RegisterHotKey(Handle, 2, System.Windows.Input.ModifierKeys.Control | System.Windows.Input.ModifierKeys.Alt | System.Windows.Input.ModifierKeys.Shift, KeyInterop.VirtualKeyFromKey(Key.S));
       RegisterHotKey(Handle, 3, System.Windows.Input.ModifierKeys.Control | System.Windows.Input.ModifierKeys.Alt | System.Windows.Input.ModifierKeys.Shift, KeyInterop.VirtualKeyFromKey(Key.Z));
+      RegisterHotKey(Handle, 4, System.Windows.Input.ModifierKeys.Control | System.Windows.Input.ModifierKeys.Alt | System.Windows.Input.ModifierKeys.Shift, KeyInterop.VirtualKeyFromKey(Key.C));
       trayIcon.Visible = true;
     }
 
-    private void CutGif(int from, int to, String filePath) {
+    private void CutGif(int from, int to, String filePath, String additionalFilter) {
       var mediaInfo = MediaInfo.Get(filePath).Result;
       var videoStream = mediaInfo.VideoStreams.First().SetSeek(TimeSpan.FromMilliseconds(from));
       var fileName = Path.GetFileNameWithoutExtension(filePath);
       var resultName = fileName + "_" + from + "_" + to + ".mp4";
       var videoPath = Environment.GetFolderPath(Environment.SpecialFolder.MyVideos);
       var resultPath = Path.Combine(videoPath, resultName);
+      var basicVf= "scale=iw*sar:ih, scale='min(800,iw)':-2";
+      var vf = ((additionalFilter.Length != 0) ? (additionalFilter+",") : "")+basicVf;
       var conv = new Conversion().AddStream(videoStream)
   .SetOutputPixelFormat(Xabe.FFmpeg.Enums.PixelFormat.Yuv420P)
   .SetInputTime(TimeSpan.FromMilliseconds(to - from))
-  .AddParameter("-vf \"scale=iw*sar:ih, scale='min(800,iw)':-2\" -crf 28 ")
+  .AddParameter($"-vf \"{vf}\" -crf 28 ")
   .SetOutput(resultPath).SetOverwriteOutput(true);
       var result = conv.Start().Result;
       if (!result.Success) {
@@ -48,6 +52,7 @@ namespace Gifer {
     int start = -1;
     int end = -1;
     String fileName = "";
+    ImageCropDialog imageCropDialog = new ImageCropDialog();
 
     protected override void WndProc(ref Message m) {
       if (m.Msg == 0x0312) {
@@ -82,13 +87,50 @@ namespace Gifer {
             break;
           case 3: {
               if (start != -1 && end != -1) {
-                CutGif(start, end, fileName);
+                CutGif(start, end, fileName,"");
+              }
+            }
+            break;
+          case 4: {
+              if (start != -1 && end != -1) {
+                ShowCropDialog();
               }
             }
             break;
         }
       }
       base.WndProc(ref m);
+    }
+
+    private void ShowCropDialog() {
+      PlayerState state = VideoPlayerAPIFactory.CreateVideoPlayerAPI().GetPlayerState();
+      if (state.position == -1) {
+        trayIcon.ShowBalloonTip(4000, "", "Can't connect to video player", ToolTipIcon.Error);
+        return;
+      }
+      String screenPath = Path.ChangeExtension(Path.GetTempFileName(), "png");
+      var result = Conversion.Snapshot(state.filePath, screenPath, TimeSpan.FromMilliseconds(state.position)).Start().Result;
+      if (!result.Success) {
+        trayIcon.ShowBalloonTip(2000, "", "Getting screenshot failed", ToolTipIcon.Error);
+        return;
+      }
+      var image = Image.FromFile(screenPath);
+      imageCropDialog.imageCropBox.Image = image;
+      imageCropDialog.imageCropBox.SelectNone();
+      var display = Screen.PrimaryScreen.WorkingArea;
+      double wScale = image.Width * 1.0 / display.Width;
+      double hScale = image.Height * 1.0 / display.Height;
+      double maxScale = Math.Max(wScale, hScale);
+      if (maxScale<=1.0) {
+        imageCropDialog.Size = image.Size;
+      } else {
+        imageCropDialog.Width = (int)(image.Width / maxScale);
+        imageCropDialog.Height = (int)(image.Height / maxScale);
+      }
+      if (imageCropDialog.ShowDialog() == DialogResult.Cancel) { return; }
+      var region = imageCropDialog.imageCropBox.SelectionRegion;
+      String crop = String.Format("crop={0}:{1}:{2}:{3}",(int)region.Width,(int)region.Height,(int)region.X, (int)region.Y);
+      CutGif(start, end, fileName, crop);
     }
 
     private void quitToolStripMenuItem_Click(object sender, EventArgs e) {
